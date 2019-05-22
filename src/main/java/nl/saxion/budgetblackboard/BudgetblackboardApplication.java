e2697981d4c160b89bca22bd0dead431f2e7d83d
PetarMihaylovski <459725@student.saxion.nl>
2019-05-22 17:29:26 +0200

    Initial commit

---
+++ src/main/java/nl/saxion/budgetblackboard/BudgetblackboardApplication.java
@@ -0,0 +1,13 @@
+package nl.saxion.budgetblackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetblackboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetblackboardApplication.class, args);
+	}
+
+}
