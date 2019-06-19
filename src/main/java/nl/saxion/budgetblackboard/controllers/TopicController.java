fe20cbbfa82a7a7ca012d74cd9da2598be4bdea2
PetarMihaylovski <459725@student.saxion.nl>
2019-06-19 11:37:46 +0200

    Adding a topic

--- src/main/java/nl/saxion/budgetblackboard/controllers/TopicController.java
+++ src/main/java/nl/saxion/budgetblackboard/controllers/TopicController.java
@@ -2,6 +2,7 @@ package nl.saxion.budgetblackboard.controllers;
 
 import nl.saxion.budgetblackboard.dataProvider.DataProvider;
 import nl.saxion.budgetblackboard.models.Subject;
+import nl.saxion.budgetblackboard.models.Topic;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,26 @@ public class TopicController {
 	private DataProvider data = DataProvider.getInstance();
 
 	@GetMapping(path = "")
-	public String getTopics(@PathVariable int subjectID, @PathVariable int courseID, Model model){
+	public String getTopics(@PathVariable int courseID, @PathVariable int subjectID,  Model model){
 		Subject currentSubject = this.data.getSubjectByID(courseID ,subjectID);
 		model.addAttribute("topics", currentSubject.getTopics());
 		model.addAttribute("course", this.data.findCourseByID(courseID));
 		model.addAttribute("subject", currentSubject);
 		return "topic/indexTopic";
+	}
+
+	@GetMapping(path = "/add")
+	public String add(@PathVariable int courseID, @PathVariable int subjectID, Model model){
+		model.addAttribute("course", this.data.findCourseByID(courseID));
+		model.addAttribute("subject", this.data.getSubjectByID(courseID, subjectID));
+		return "topic/addTopic";
+	}
+
+	@PostMapping(path = "/add")
+	public String addTopic(@PathVariable int courseID, @PathVariable int subjectID, Model model, Topic topic){
+		Subject currentSubject = this.data.getSubjectByID(courseID, subjectID);
+		currentSubject.addTopic(topic);
+		model.addAttribute("topics", currentSubject.getTopics());
+		return "redirect:/courses/"  + courseID + "/subjects/" + subjectID;
 	}
 }
