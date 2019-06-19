d43f948b4f1a4cb0cfd4e3c038bd6fd71f91813f
PetarMihaylovski <459725@student.saxion.nl>
2019-06-19 02:52:18 +0200

    Fragments fixed

---
+++ src/main/java/nl/saxion/budgetblackboard/controllers/TopicController.java
@@ -0,0 +1,13 @@
+package nl.saxion.budgetblackboard.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/courses/{courseID}/subjects/{subjectID}")
+public class TopicController {
+/*
+	@GetMapping(path = "")
+	public String*/
+}
